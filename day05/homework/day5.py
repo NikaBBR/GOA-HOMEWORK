e63394de2d3bfbf7073f8444a858e0cfe4b0e8a2
NikaBBR <taktakidzenikoloz@gmail.com>
2024-02-22 19:45:45 +0400

    second commit

---
+++ day05/homework/day5.py
@@ -0,0 +1,52 @@
+print= 5
+5+NIKa=print
+
+print=nika aris 10 wlis
+
+
+
+print(nika"
+      
+
+
+
+
+
+print(my age = nika  
+
+print= nika da nika 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
